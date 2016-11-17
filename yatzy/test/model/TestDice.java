ea79a42f0d5fb8c4860c5ec874d6972659e18b7a
Christer Hamberg <CADF@ad.spel.se>
2016-11-17 20:22:51 +0100

    RED 3rd test case is not working

--- yatzy/test/model/TestDice.java
+++ yatzy/test/model/TestDice.java
@@ -23,6 +23,23 @@ public class TestDice {
 		
 	}
 	
+	@Test
+	public void testRollDice (){
+		
+		int DICE_LOW = 1;
+		int DICE_HIGH = 6;
+		
+		Dice dc = new Dice ();
+		
+		int value = dc.rollDice ();
+		
+		if (value>=DICE_LOW) assertTrue(true);
+		else fail ("Value of dice to low");
+		
+		if (value<=DICE_HIGH) assertTrue (true);
+		else fail ("Value of dice to high");
+		
+	}
 
 
 }

f3988c42eaba9a5b9f6e3b3b203bee346904795b
Christer Hamberg <CADF@ad.spel.se>
2016-11-17 19:52:50 +0100

    GREEN - Output hook is working

--- yatzy/src/yatzy/YatzyMain.java
+++ yatzy/src/yatzy/YatzyMain.java
@@ -4,6 +4,11 @@ public class YatzyMain {
 	
 	public static void main(String[] args) {
 		
+		displayWelcome ();
+		
 	}
-	
+
+	public static void displayWelcome (){
+		System.out.print("\nLets play a game of Yatzy!");
+	}
 }

7271cb676bafcb124bfab1db7f9fb6fabf6d85eb
Christer Hamberg <CADF@ad.spel.se>
2016-11-17 20:13:06 +0100

    GREEN 2nd test case is now working

--- yatzy/src/model/Dice.java
+++ yatzy/src/model/Dice.java
@@ -2,5 +2,16 @@ package model;
 
 public class Dice {
 	
-
+	// State of the dice
+	boolean frozen = false;
+	
+	
+    public boolean isFrozen (){
+    	return frozen;
+    }
+    
+    public void freezeDice (){
+    	frozen = true;
+    }
+    
 }
--- yatzy/test/model/TestDice.java
+++ yatzy/test/model/TestDice.java
@@ -9,12 +9,16 @@ public class TestDice {
 	@Test
 	public void testFreezeDiceIsFrozen() {
 		
+		// Create a dice
 		Dice dc = new Dice ();
 		
+		// Check that it by default is unfrozen
 		assertFalse (dc.isFrozen());
 		
+		// Freeze the dice
 		dc.freezeDice ();
 		
+		// Check that it is frozen
 		assertTrue (dc.isFrozen());
 		
 	}

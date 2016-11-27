e6b80b084c9bfb1ea782e01e4b1d33e7b14b17b1
Christer Hamberg <CADF@ad.spel.se>
2016-11-27 14:36:51 +0100

    RED updated the test case

--- yatzy/test/model/TestViewGui.java
+++ yatzy/test/model/TestViewGui.java
@@ -13,6 +13,9 @@ public class TestViewGui {
 		
 		ViewGui gui = new ViewGui ("Test1");
 		assertEquals (gui.getTitle(),"Test1");
+
+		ViewGui gui2 = new ViewGui ("Lets play a game of Yatzy!");
+		assertEquals (gui2.getTitle(),"Lets play a game of Yatzy!");
 		
 	}
 

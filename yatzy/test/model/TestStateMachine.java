98f24549d5b8748e4632aa11277ee15303c5a538
Christer Hamberg <CADF@ad.spel.se>
2016-11-27 20:53:49 +0100

    RED

---
+++ yatzy/test/model/TestStateMachine.java
@@ -0,0 +1,35 @@
+package model;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestStateMachine {
+
+	@Test
+	public void testIsActive() {
+
+		StateMachine mc = new StateMachine();
+		assertFalse (mc.isActive());
+		
+	}
+	
+	@Test
+	public void testGetStepRollState() {
+		
+		StateMachine mc = new StateMachine();
+		assertFalse (mc.isActive(),false);
+		assertEquals (mc.getRollState(),0);
+		mc.stepRollState();
+		assertEquals (mc.getRollState(),1);
+		mc.stepRollState();
+		assertEquals (mc.getRollState(),2);
+		mc.stepRollState();
+		assertEquals (mc.getRollState(),3);
+		mc.stepRollState();
+		assertEquals (mc.getRollState(),1);
+
+	}
+	
+	
+}

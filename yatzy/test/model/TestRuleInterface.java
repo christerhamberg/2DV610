d37f3ff4f79c4f24c8f7f856173ecfbe6a14567f
Christer Hamberg <CADF@ad.spel.se>
2016-11-28 19:36:09 +0100

    GREEN

--- yatzy/test/model/TestRuleInterface.java
+++ yatzy/test/model/TestRuleInterface.java
@@ -32,5 +32,126 @@ public class TestRuleInterface {
 		assertEquals (rule.getResult(),5);
 	
 	}
+	
+	@Test
+	public void testRuleOnly2() {
 
+		RuleInterface rule = new RuleOnlyTwos ();
+		assertEquals (rule.getRuleDescription(),"Max number of 2s");
+
+		assertEquals (rule.getResult(),0);
+		
+		assertEquals (rule.validateResult(2,1,3,4,5),2);
+		assertEquals (rule.getResult(),2);
+
+		rule.resetResult();
+		assertEquals (rule.getResult(),0);
+		
+		assertEquals (rule.validateResult(1,2,3,4,5),2);
+		assertEquals (rule.getResult(),2);
+		rule.resetResult();
+		assertEquals (rule.getResult(),0);
+		
+		assertEquals (rule.validateResult(2,2,2,2,2),10);
+		assertEquals (rule.getResult(),10);
+	
+	}
+	
+	@Test
+	public void testRuleOnly3() {
+
+		RuleInterface rule = new RuleOnlyThrees ();
+		assertEquals (rule.getRuleDescription(),"Max number of 3s");
+
+		assertEquals (rule.getResult(),0);
+		
+		assertEquals (rule.validateResult(3,1,2,4,5),3);
+		assertEquals (rule.getResult(),3);
+
+		rule.resetResult();
+		assertEquals (rule.getResult(),0);
+		
+		assertEquals (rule.validateResult(1,2,3,4,5),3);
+		assertEquals (rule.getResult(),3);
+		rule.resetResult();
+		assertEquals (rule.getResult(),0);
+		
+		assertEquals (rule.validateResult(3,3,3,3,3),15);
+		assertEquals (rule.getResult(),15);
+	
+	}
+	
+	@Test
+	public void testRuleOnly4() {
+
+		RuleInterface rule = new RuleOnlyFours ();
+		assertEquals (rule.getRuleDescription(),"Max number of 4s");
+
+		assertEquals (rule.getResult(),0);
+		
+		assertEquals (rule.validateResult(3,1,2,4,5),4);
+		assertEquals (rule.getResult(),4);
+
+		rule.resetResult();
+		assertEquals (rule.getResult(),0);
+		
+		assertEquals (rule.validateResult(4,2,3,1,5),4);
+		assertEquals (rule.getResult(),4);
+		rule.resetResult();
+		assertEquals (rule.getResult(),0);
+		
+		assertEquals (rule.validateResult(4,4,4,4,4),20);
+		assertEquals (rule.getResult(),20);
+	
+	}
+
+	
+	@Test
+	public void testRuleOnly5() {
+
+		RuleInterface rule = new RuleOnlyFives ();
+		assertEquals (rule.getRuleDescription(),"Max number of 5s");
+
+		assertEquals (rule.getResult(),0);
+		
+		assertEquals (rule.validateResult(3,1,2,4,5),5);
+		assertEquals (rule.getResult(),5);
+
+		rule.resetResult();
+		assertEquals (rule.getResult(),0);
+		
+		assertEquals (rule.validateResult(5,2,3,1,6),5);
+		assertEquals (rule.getResult(),5);
+		rule.resetResult();
+		assertEquals (rule.getResult(),0);
+		
+		assertEquals (rule.validateResult(5,5,5,5,5),25);
+		assertEquals (rule.getResult(),25);
+	
+	}
+	
+	@Test
+	public void testRuleOnly6() {
+
+		RuleInterface rule = new RuleOnlySixes ();
+		assertEquals (rule.getRuleDescription(),"Max number of 6s");
+
+		assertEquals (rule.getResult(),0);
+		
+		assertEquals (rule.validateResult(3,1,2,4,6),6);
+		assertEquals (rule.getResult(),6);
+
+		rule.resetResult();
+		assertEquals (rule.getResult(),0);
+		
+		assertEquals (rule.validateResult(6,2,3,1,2),6);
+		assertEquals (rule.getResult(),6);
+		rule.resetResult();
+		assertEquals (rule.getResult(),0);
+		
+		assertEquals (rule.validateResult(6,6,6,6,6),30);
+		assertEquals (rule.getResult(),30);
+	
+	}
+	
 }

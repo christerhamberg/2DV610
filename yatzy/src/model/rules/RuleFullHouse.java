7f9e2a6735875ff09dd033dfd797631c1895f4dd
Christer Hamberg <ch222xb@student.lnu.se>
2016-11-29 20:20:28 +0100

    GREEN

---
+++ yatzy/src/model/rules/RuleFullHouse.java
@@ -0,0 +1,69 @@
+package model.rules;
+
+public class RuleFullHouse implements RuleInterface{
+	
+	private String ruleDescription = "Full House";
+	private int result = 0;
+	
+	@Override
+	public String getRuleDescription() {
+		return ruleDescription;
+	}
+
+	@Override
+	public int validateResult(int dice1, int dice2, int dice3, int dice4, int dice5) {
+
+		int diceFound[] = {0,0,0,0,0,0};
+		boolean pairFound = false;
+		int pairValue = 0;
+		boolean trissFound = false;
+		int trissValue = 0;
+		
+		int tempResult = 0;
+		
+		for (int loopMe=1;6>=loopMe;loopMe++){
+			
+			if (dice1 == loopMe) diceFound[loopMe-1] = diceFound[loopMe-1] +1;
+			if (dice2 == loopMe) diceFound[loopMe-1] = diceFound[loopMe-1] +1;;
+			if (dice3 == loopMe) diceFound[loopMe-1] = diceFound[loopMe-1] +1;;
+			if (dice4 == loopMe) diceFound[loopMe-1] = diceFound[loopMe-1] +1;;
+			if (dice5 == loopMe) diceFound[loopMe-1] = diceFound[loopMe-1] +1;;
+
+		}
+		
+		for (int loopMe=0; diceFound.length>loopMe;loopMe++){
+			
+			if (diceFound[loopMe] == 2) {
+				pairFound = true;
+				pairValue = loopMe+1;
+			}
+			
+			if (diceFound[loopMe] == 3) {
+				trissFound = true;
+				trissValue = loopMe+1;
+			}
+			
+		}
+		
+		if (pairFound == true){
+			if (trissFound == true){
+				tempResult = pairValue *2 + trissValue*3;
+			}
+		}
+		
+		result = tempResult;
+		return result;
+		
+	}
+
+	@Override
+	public int getResult() {
+		return result;
+	}
+
+	@Override
+	public void resetResult() {
+		result = 0;
+	}
+
+}
